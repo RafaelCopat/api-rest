308d2e7701dde5f7f21564030f25eb9d4ee94908
RafaelCopat <rfc904@gmail.com>
2022-02-05 16:13:12 -0300

    final project

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 
+	"github.com/rafaelcopat/go-rest-api/database"
 	"github.com/rafaelcopat/go-rest-api/models"
 	"github.com/rafaelcopat/go-rest-api/routes"
 )
 
 func main() {
 	models.Personalidades = []models.Personalidade{
-		{Nome: "Nome 1", Historia: "Historia 1"},
-		{Nome: "Nome 2", Historia: "Historia 2"},
+		{ID: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{ID: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
+
+	database.ConectaComBancoDeDados()
+
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
